45698cf6f9dfb40bccd55e540895969bff7894e3
Giovane Possebon <giovane.possebon@iwsbrazil.com>
2017-08-11 23:14:10 -0300

    Home screen implemented

---
+++ AppOfMyLife/AppOfMyLife/Home/Cells/NextEpisodeSection.swift
@@ -0,0 +1,19 @@
+//
+//  NextEpisodeSection.swift
+//  AppOfMyLife
+//
+//  Created by Giovane Possebon on 11/8/17.
+//  Copyright © 2017 giovane. All rights reserved.
+//
+
+import UIKit
+
+class NextEpisodeSection: UITableViewCell {
+    
+    static let nibName = "NextEpisodeSection"
+    static let identifier = "NextEpisodeSection"
+    static let height: CGFloat = 45
+    
+    @IBOutlet weak var labelTitle: UILabel!
+    
+}
--- AppOfMyLife/AppOfMyLife/Home/HomeViewController.swift
+++ AppOfMyLife/AppOfMyLife/Home/HomeViewController.swift
@@ -32,6 +32,7 @@ class HomeViewController: UITableViewController {
     
     private func setupTableView() {
         tableView.register(UINib(nibName: NextEpisodeCell.nibName, bundle: nil), forCellReuseIdentifier: NextEpisodeCell.identifier)
+        tableView.register(UINib(nibName: NextEpisodeSection.nibName, bundle: nil), forCellReuseIdentifier: NextEpisodeSection.identifier)
         tableView.separatorStyle = .none
     }
     
@@ -52,7 +53,7 @@ extension HomeViewController: HomeView {
     }
 }
 
-// MARK: TableViewDelegate
+// MARK: TableViewDatSource
 
 extension HomeViewController {
     
@@ -60,19 +61,13 @@ extension HomeViewController {
         return schedule?.count ?? 0
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return NextEpisodeCell.height
-    }
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: NextEpisodeCell.identifier, for: indexPath) as? NextEpisodeCell {
-            let item = schedule?[indexPath.row]
-            
-            cell.labelTitle.text = item?.episode?.title
-            cell.labelSubtitle.text = item?.show?.title
-            cell.imgBackground.image = UIImage(named: "loginBackground")
-            
-            return cell
+            if let item = schedule?[indexPath.row] {
+                cell.populate(withSchedule: item)
+                
+                return cell
+            }
         }
         
         return UITableViewCell()
@@ -83,5 +78,23 @@ extension HomeViewController {
 // MARK: TableViewDelegate
 
 extension HomeViewController {
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return NextEpisodeCell.height
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return NextEpisodeSection.height
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: NextEpisodeSection.identifier) as? NextEpisodeSection {
+            cell.labelTitle.text = "In the next episode..."
+            
+            return cell
+        }
+        
+        return UIView()
+    }
     
 }
